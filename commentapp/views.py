259a95a947692a72c1478cc5c5c4869df74171a3
younghunei <cyh5833@naver.com>
2024-04-22 11:10:57 +0900

    make view mobile

--- commentapp/views.py
+++ commentapp/views.py
@@ -21,7 +21,7 @@ class CommentCreateView(CreateView):
     def form_valid(self, form):
 
         temp_comment = form.save(commit=False)
-        temp_comment.article = Article.objects.get(pk=self.request.POST['article.pk'])
+        temp_comment.article = Article.objects.get(pk=self.request.POST['article_pk'])
         temp_comment.writer = self.request.user
         temp_comment.save()
 
